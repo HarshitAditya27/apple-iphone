2258a529f1bdabfa19c7d479883fda011add59a6
Harshit Aditya <harshitaditya27@gmail.com>
2024-03-16 19:57:51 +0530

    Added initial setup files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <main className="bg-blck">
+        <Navbar />
+        <Hero />
+        <Highlights />
+      </main>
+    </>
+  );
+}
+
+export default App;
